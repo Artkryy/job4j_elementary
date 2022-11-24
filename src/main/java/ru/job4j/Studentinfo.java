48a3126f031e292a03951c665bbe8e1042bab5ca
Artkryy <zzqwezz8@gmail.com>
2022-11-24 16:35:41 +0300

    3.3 Фиксация изменений на github [#243564]

---
+++ src/main/java/ru/job4j/Studentinfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Studentinfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Kryakvin");
+        System.out.println("13.11.1997");
+
+    }
+}
